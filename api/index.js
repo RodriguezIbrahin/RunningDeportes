1a48a9667e6260d08c362c6980dd695ed1a34d0c
dariovelazquez7 <dario.velazquez10@gmail.com>
2021-03-15 22:12:49 -0300

    ready

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-    app.listen(3002, () => {
+    app.listen(process.env.PORT, () => {
     console.log('%s listening at 3002'); // eslint-disable-line no-console
   });
 });
