16651530d70ba17fd5d24f308a6e17b35163d8ea
dariovelazquez7 <dario.velazquez10@gmail.com>
2021-04-07 06:04:59 -0300

    lista en producto de talles arreglada

--- client/src/component/products/Psize.js
+++ client/src/component/products/Psize.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import { bindActionCreators } from 'redux';
 import * as actionCreators from "../../redux/Actions";
 import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab';
-import {Button, Typography} from '@material-ui/core';
+import {Button, Typography, Grid} from '@material-ui/core';
 import AddShoppingCartOutlinedIcon from '@material-ui/icons/AddShoppingCartOutlined';
 import { AlertError, AlertSucess } from "../Alerts";
 
@@ -48,19 +48,57 @@ function Psize({ id, props, UpDownProductCart, CartNoRegister}) {
         <Typography align="center" variant="body2" style={{ fontWeight: 'bold', fontSize: '1rem'}} gutterBottom> 
            
             <br/><i>Talles</i><br/><br/>
+
+            { props && props.length < 7 ?
             
-            <ToggleButtonGroup value={size} exclusive onChange={handleSize} aria-label="size" > 
+                <ToggleButtonGroup value={size} exclusive onChange={handleSize} aria-label="size" > 
 
-                {props && props.map( size => 
+                    {props && props.map( size => 
 
-                    <ToggleButton value={size.ar} aria-label={size.ar}>
-                        {size.ar}
-                    </ToggleButton>
-                )}
-            </ToggleButtonGroup>
+                        <ToggleButton value={size.ar} aria-label={size.ar}>
+                           {size.ar}
+                        </ToggleButton>
+                    )}
+                </ToggleButtonGroup>
+                : 
+                <Grid item container direction="row" justify="center" alignItems="center" xs ={12}>
+
+                    <Grid item container direction="row" justify="center" alignItems="center" xs ={10}>
+
+                        <ToggleButtonGroup value={size} exclusive onChange={handleSize} aria-label="size" > 
+
+                            {props && props.slice(0, 6).map( size => 
+
+                                <ToggleButton value={size.ar} aria-label={size.ar}>
+                                    {size.ar}
+                                </ToggleButton>
+                            )}
+
+                        </ToggleButtonGroup>
+
+                    </Grid>
+
+                    <Grid item container direction="row" justify="center" alignItems="center" xs ={10}>
+
+                        <ToggleButtonGroup value={size} exclusive onChange={handleSize} aria-label="size" > 
+
+                            {props && props.slice(6, props.length).map( size => 
+
+                                <ToggleButton value={size.ar} aria-label={size.ar}>
+                                   {size.ar}
+                                </ToggleButton>
+                            )}
+
+                        </ToggleButtonGroup>
+
+                    </Grid>
+
+                </Grid>
+            }
 
             <br/>
             <AlertError props={error} onClick={setError}/>
+
             <AlertSucess props={succes} onClick={setSucces}/>
             <br/>
 
